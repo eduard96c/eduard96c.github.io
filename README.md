0cd5fb3566cbe0f025f693ce458f24a3569317a0
fprophet <ciubaneduard1196@gmail.com>
2023-11-27 05:50:05 -0800

    js file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # eduard96c.github.io
+
+##Live
+
+[eduard96c.github.io](https://eduard96c.github.io/)
---
+++ examples.js
@@ -0,0 +1,5 @@
+var employed = true;
+console.log(employed);
+
+var job = document.getElementById("job-title");
+console.log(job);
