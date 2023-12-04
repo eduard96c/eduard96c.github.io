96ab9c0ecc6421efa6ef5877d4a48500fb7b651e
eduard96c <ciubaneduard1196@gmail.com>
2023-12-04 09:56:53 -0800

    commented js code

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ function nav() {
 }
 
 function _handleNavClick() {
+  //current selected nav-item
   selected = document.querySelector(".selected");
+  //current container that is displayed
   current_container = document.getElementById("main-" + selected.id);
+  //next container to be displayed
   next_container = document.getElementById("main-" + this.id);
+
   if (this !== selected) {
+    //hide the current container
     current_container.classList.add("d-none");
+    //display the next container
     next_container.classList.remove("d-none");
+    //remove the "selected" class from the nav-item
     selected.classList.remove("selected");
+    //add the "selected" class to the clicked nav-item
     this.classList.add("selected");
   }
 }
