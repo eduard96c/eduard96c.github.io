b61bd51b76dc135a846090656b5930ebee8406da
eduard96c <ciubaneduard1196@gmail.com>
2023-12-17 12:03:48 -0800

    added text and style to pages

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
+active_page = "home";
+current_container = document.getElementById("main-" + active_page);
+selected = document.getElementById(active_page);
+
 function nav() {
+  current_container.classList.add("d-block");
+  selected.classList.add("selected");
+
   var btns = document.querySelectorAll(".nav-item");
   btns.forEach(function (elem) {
     elem.addEventListener("click", _handleNavClick);
   });
 }
 
-function _handleNavClick() {
-  //current selected nav-item
-  selected = document.querySelector(".selected");
-  //current container that is displayed
-  current_container = document.getElementById("main-" + selected.id);
-  //next container to be displayed
-  next_container = document.getElementById("main-" + this.id);
+function _handleNavClick(e) {
+  current_container = document.getElementById("main-" + active_page);
 
-  if (this !== selected) {
-    //hide the current container
-    current_container.classList.add("d-none");
-    //display the next container
-    next_container.classList.remove("d-none");
-    //remove the "selected" class from the nav-item
+  active_page = e.target.id;
+
+  selected = document.querySelector(".selected");
+
+  next_container = document.getElementById("main-" + active_page);
+
+  if (e.target !== selected) {
+    current_container.classList.remove("d-block");
+    next_container.classList.add("d-block");
     selected.classList.remove("selected");
-    //add the "selected" class to the clicked nav-item
     this.classList.add("selected");
   }
 }
