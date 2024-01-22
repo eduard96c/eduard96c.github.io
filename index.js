32aad51947a614b99082939e09c9064bdc9cdf35
eduard96c <ciubaneduard1196@gmail.com>
2024-01-22 07:25:07 -0800

    js changes

--- index.js
+++ index.js
@@ -34,28 +34,23 @@ function _handleNavClick(e) {
   }
 }
 
-function list_select() {
-  let lists = document.querySelectorAll(".list-select");
-  lists.forEach(function (elem) {
-    elem.addEventListener("click", function (ev) {
-      var page = document.querySelector("#" + active_page);
-      if (ev.target.nodeName.toLowerCase() == "li") {
-        page_lists = page.querySelectorAll(".data-list");
-        list_selectors = page.querySelectorAll(".list-select li");
-        //remove selected class
-        list_selectors.forEach(function (sl) {
-          sl.classList.remove("selected-list");
-        });
-        //hide other lists
-        page_lists.forEach(function (pg) {
-          pg.style.display = "none";
-        });
-        ev.target.classList.add("selected-list");
-        page.querySelector("#" + ev.target.dataset.type).style.display =
-          "block";
-      }
+function _handleListChange(ev) {
+  console.log(event);
+  var page = document.querySelector("#" + active_page);
+  if (event.target.nodeName.toLowerCase() == "li") {
+    page_lists = page.querySelectorAll(".data-list");
+    list_selectors = page.querySelectorAll(".list-select li");
+    //remove selected class
+    list_selectors.forEach(function (sl) {
+      sl.classList.remove("selected-list");
     });
-  });
+    //hide other lists
+    page_lists.forEach(function (pg) {
+      pg.style.display = "none";
+    });
+    event.target.classList.add("selected-list");
+    page.querySelector("#" + event.target.dataset.type).style.display = "block";
+  }
 }
 
 async function fetch_json(path) {
@@ -69,40 +64,47 @@ async function fetch_json(path) {
   return response;
 }
 async function showSkills() {
-  var page_section = $("#skills section:last-child");
+  var page_list_section = $("#skills section:last-child");
+  var page_menu_section = $("#skills section:first-child");
+  menu_html = "<ul class='list-select' onclick='_handleListChange(this)'>";
+
   let skills = [];
 
   skills = await fetch_json("./skills.json");
 
   skills.forEach(function (category) {
     style = "";
+    menu_class = "";
     if (category.main !== undefined) {
       style = "style='display:block'";
+      menu_class = "class='selected-list'";
     }
+
+    menu_html += `<li ${menu_class} data-type='${category.type.toLowerCase()}'>${
+      category.type
+    }</li>`;
+
     html = `<ul id='${category.type.toLowerCase()}' class='data-list skill-list' ${style}><h3>${
       category.type
     } Skills</h3>`;
+
     var response = category.list.map(function (skill) {
+      fav_class = "";
       if (skill.favourite) {
-        return (
-          "<li class='favourite'>" +
-          skill.name +
-          "-<span>" +
-          skill.details +
-          "</span></li>"
-        );
-      } else {
-        return "<li>" + skill.name + "-<span>" + skill.details + "</span></li>";
+        fav_class = "class='favourite'";
       }
+      response = `<li ${fav_class}> <span>${skill.name}</span> - <span>${skill.details}</span></li>`;
+      return response;
     });
-    // console.log(category);
-    console.log(response);
+
     html += response.join("");
+    console.log(response);
     html += "</ul>";
-    page_section.innerHTML += html;
-    page_section;
+
+    page_list_section.innerHTML += html;
   });
+  menu_html += "</ul>";
+  page_menu_section.innerHTML += menu_html;
 }
 showSkills();
 nav();
-list_select();
